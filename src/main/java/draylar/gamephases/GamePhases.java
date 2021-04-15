083b0a8228311eb37b2b6fba9da951c9e9a28973
Draylar <samuelhimes@gmail.com>
2021-04-15 17:39:45 -0500

    initial commit

---
+++ src/main/java/draylar/gamephases/GamePhases.java
@@ -0,0 +1,10 @@
+package draylar.gamephases;
+
+import net.fabricmc.api.ModInitializer;
+
+public class GamePhases implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/draylar/gamephases/GamePhasesClient.java
@@ -0,0 +1,13 @@
+package draylar.gamephases;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class GamePhasesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
