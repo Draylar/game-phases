e8dec89233d76b2826db67cbc0b8d2ce6b4e2ca6
Draylar <samuelhimes@gmail.com>
2021-04-16 16:05:41 -0500

    update /phase command to take a player target argument

--- src/main/java/draylar/gamephases/command/PhaseCommand.java
+++ src/main/java/draylar/gamephases/command/PhaseCommand.java
@@ -5,10 +5,10 @@ import com.mojang.brigadier.tree.LiteralCommandNode;
 import draylar.gamephases.GamePhases;
 import draylar.gamephases.kube.GamePhasesEventJS;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
-import net.minecraft.command.argument.IdentifierArgumentType;
+import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.util.Identifier;
 
 public class PhaseCommand {
 
@@ -19,29 +19,33 @@ public class PhaseCommand {
                     .build();
 
             LiteralCommandNode<ServerCommandSource> grant = CommandManager.literal("grant")
+                    .then(CommandManager.argument("player", EntityArgumentType.player())
                     .then(CommandManager.argument("phase", StringArgumentType.word())
                             .suggests((context, builder) -> {
                                 GamePhasesEventJS.getPhases().keySet().forEach(builder::suggest);
                                 return builder.buildFuture();
                             })
                             .executes(context -> {
+                                PlayerEntity target = EntityArgumentType.getPlayer(context, "player");
                                 String phase = StringArgumentType.getString(context, "phase");
-                                GamePhases.getPhaseData(context.getSource().getPlayer()).set(phase, true);
+                                GamePhases.getPhaseData(target).set(phase, true);
                                 return 1;
-                            }))
+                            })))
                     .build();
 
             LiteralCommandNode<ServerCommandSource> revoke = CommandManager.literal("revoke")
+                    .then(CommandManager.argument("player", EntityArgumentType.player())
                     .then(CommandManager.argument("phase", StringArgumentType.word())
                             .suggests((context, builder) -> {
                                 GamePhasesEventJS.getPhases().keySet().forEach(builder::suggest);
                                 return builder.buildFuture();
                             })
                             .executes(context -> {
+                                PlayerEntity target = EntityArgumentType.getPlayer(context, "player");
                                 String phase = StringArgumentType.getString(context, "phase");
-                                GamePhases.getPhaseData(context.getSource().getPlayer()).set(phase, false);
+                                GamePhases.getPhaseData(target).set(phase, false);
                                 return 1;
-                            }))
+                            })))
                     .build();
 
             root.addChild(revoke);
